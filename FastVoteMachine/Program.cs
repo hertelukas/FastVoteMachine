cacb5dce0354b0eef2fcdd3ccd359d7b5b6ead12
lukas <luki.hertel@gmail.com>
2022-03-23 15:26:47 +0100

    Cleaned up the starting

--- FastVoteMachine/Program.cs
+++ FastVoteMachine/Program.cs
@@ -22,7 +22,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    Console.WriteLine("Running in Release Environment");
     app.UseExceptionHandler("/Error");
 
     // Configure for web hosting
@@ -31,10 +30,12 @@ if (!app.Environment.IsDevelopment())
         ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
     });
 
-    app.UseAuthentication();
-
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
